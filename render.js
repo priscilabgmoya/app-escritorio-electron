eb46e3ca3aeea531ee7d15796c9474f4a8d64a0c
priscilabgmoya <105519676+priscilabgmoya@users.noreply.github.com>
2023-12-26 22:13:41 -0300

    se agrega funcionalidad de contador

--- render.js
+++ render.js
@@ -1,6 +1,16 @@
 const count = document.getElementById("count"),
-btnClickMe = document.getElementById("btnClickMe"); 
+btnClickMe = document.getElementById("btnClickMe"), 
+btnDelete = document.getElementById("btnDelete"),
+btnReset = document.getElementById("btnReset"); 
 
 btnClickMe.addEventListener("click", () =>{
   count.innerText = parseInt(count.innerText) +1; 
+})
+btnReset.addEventListener("click", () =>{
+  count.innerText = 0; 
+})
+btnDelete.addEventListener("click", () =>{
+  if(parseInt( count.innerText) > 0){
+    count.innerText =  parseInt(count.innerText) - 1; 
+  }
 })
